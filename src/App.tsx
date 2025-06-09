99faa48ce345527e6c90a1b17a62cb4426a83b42
maksim.samorukov <maksim.samorukov@sphaera.ru>
2025-06-09 23:33:08 +0300

    added some logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,80 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import s from "./App.module.scss";
+import cn from "classnames";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [firstLine, setFirstLine] = useState<string>();
+  const [secondLine, setSecondLine] = useState<string>();
+  const [warning, setWarning] = useState<boolean>(false);
+  const handleInput =
+    (line: "first" | "second") => (e: React.ChangeEvent<HTMLInputElement>) => {
+      if (line === "first") {
+        setFirstLine(e.target.value);
+      }
+      if (line === "second") {
+        setSecondLine(e.target.value);
+      }
+      if (secondLine?.length !== firstLine?.length) {
+        setWarning(true);
+      } else {
+        setWarning(false);
+      }
+    };
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className={s.container}>
+      <div className={s.inputsContainer}>
+        <input
+          value={firstLine}
+          onChange={handleInput("first")}
+          className={s.input}
+        />
+        <input
+          value={secondLine}
+          onChange={handleInput("second")}
+          className={s.input}
+        />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div className={s.warningContainer}>
+        {warning && (
+          <div className={s.warningText}>
+            Длина введенных последовательностей различна
+          </div>
+        )}
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      <div className={s.resultContainer}>
+        <div className={s.resultFirstLine}>
+          {firstLine?.split("").map((i) => (
+            <span className={cn(s[`${i.toUpperCase()}`], s.firstLineLetter)}>
+              {i}
+            </span>
+          ))}
+        </div>
+        <div className={s.resultSecondLine}>
+          {secondLine?.split("").map((i, index) => {
+            console.log(
+              ">>",
+              i.toLowerCase(),
+              ">",
+              firstLine?.charAt(index),
+              "index",
+              index
+            );
+            if (i.toLowerCase() !== firstLine?.charAt(index)) {
+              return (
+                <span
+                  className={cn(s[`${i.toUpperCase()}`], s.firstLineLetter)}
+                >
+                  {i}
+                </span>
+              );
+            } else {
+              return <span className={s.secondLineLetter}>{i}</span>;
+            }
+          })}
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
